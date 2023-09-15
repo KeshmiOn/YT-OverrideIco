6eb348853efef122384205ca17b313bb6016d902
KeshmiOn <136059042+KeshmiOn@users.noreply.github.com>
2023-09-15 12:25:25 +0300

    Create YT-OverrideIco.user.js

---
+++ YT-OverrideIco.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        YT-OverrideIco
+// @namespace   Violentmonkey Scripts
+// @version     1.0
+// @author      Keshmi
+// @description:ru Переопределяет ресурс иконок для Youtube. 
+// @match       https://youtube.com/*
+// @match       https://www.youtube.com/*
+// @description 15.09.2023
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var resourceURL = "https://yt3.ggpht.com/";
+    var redirectURL = "https://yt4.ggpht.com/";
+
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === "childList") {
+                var replacedElements = document.querySelectorAll('img[src^="' + resourceURL + '"]');
+                replacedElements.forEach(function(element) {
+                    var originalURL = element.getAttribute('src');
+                    if (originalURL.startsWith(resourceURL)) {
+                        var newURL = originalURL.replace(resourceURL, redirectURL);
+                        element.setAttribute('src', newURL);
+                    }
+                });
+            }
+        });
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+})();
